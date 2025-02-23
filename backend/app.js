7d9566c5439f3fc14e10e67548ba4b26702935ea
VikasSingh30 <singhvikas.sv30@gmail.com>
2025-02-23 17:43:01 +0530

    profile

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const userRouter = require('./Routes/user.routes.js');
+const userRouter = require('./routes/user.routes.js');
 
 const connectToDB = require('./db/db');
 connectToDB();
@@ -12,7 +13,7 @@ connectToDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
