31d569d4b4e8d8514e7624f4edd4b79af8eb555a
Vikas <singhvikas.sv30@gmail.com>
2025-05-11 13:15:08 +0000

    context,wrapper,logout,signup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,             // Allow external access
+    strictPort: false,
+    allowedHosts: [
+      "5173-vikassingh30-uberapp-8enmbt6jk2q.ws-us118.gitpod.io",
+      "5174-vikassingh30-uberapp-8enmbt6jk2q.ws-us118.gitpod.io"
+    ],
+  },
 })
+
